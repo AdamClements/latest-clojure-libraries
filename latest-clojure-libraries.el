0ca590f176dd949a8fc5d32c34fb70a596ad1d24
Adam Clements <adam.clements@gmail.com>
2013-10-23 13:43:10 +0100

    Make the nrepl dependency entirely optional (not just a case of whether it's running or not, the module is no longer required in emacs)

--- latest-clojure-libraries.el
+++ latest-clojure-libraries.el
@@ -5,8 +5,7 @@
 ;; Plugin:  Latest clojure libraries dependency resolver
 ;; Author:  Adam Clements <adam.clements@gmail.com>
 ;; URL:     http://github.com/AdamClements/latest-clojure-libraries/
-;; Package-Requires: ((nrepl "0.2.0"))
-;; Version: 0.4
+;; Version: 0.5
 ;; License: Eclipse Public License
 
 ;;; Commentary:
@@ -20,7 +19,9 @@
 
 ;;; Code:
 
-(require 'nrepl)
+(setq lcl/nrepl-enabled? (condition-case nil
+                             (require 'nrepl)
+                           (error nil)))
 
 (defun lcl/get-version-vec (s)
   (when (string-match "[[].+? \".+?\"[]]" s)
@@ -32,19 +33,23 @@
     (format "lein ancient latest %s" package package))))
 
 (defun lcl/add-clojure-dependency (spec)
-  (nrepl-send-string
-   (concat "(require 'cemerick.pomegranate)"
-           "(cemerick.pomegranate/add-dependencies"
-           "  :coordinates '[" spec "]"
-           "  :repositories (merge cemerick.pomegranate.aether/maven-central"
-           "                       {\"clojars\" \"http://clojars.org/repo\"}))")
-   (lambda (out) (message (print out)))))
+  (when lcl/nrepl-enabled?
+    (nrepl-send-string
+     (concat "(require 'cemerick.pomegranate)"
+             "(cemerick.pomegranate/add-dependencies"
+             "  :coordinates '[" spec "]"
+             "  :repositories (merge cemerick.pomegranate.aether/maven-central"
+             "                       {\"clojars\" \"http://clojars.org/repo\"}))")
+     (lambda (out) (message (print out))))))
+
+(defun lcl/nrepl-available ()
+  (and lcl/nrepl-enabled? (nrepl-current-repl-buffer)))
 
 ;;;###autoload
 (defun latest-clojure-libraries-insert-dependency (package inject)
   "Insert dependency for PACKAGE and optionally INJECT it into nrepl classpath."
   (interactive (list (read-string "Library name: ")
-                     (when (nrepl-current-repl-buffer)
+                     (when (lcl/nrepl-available)
                        (y-or-n-p "Add to running nrepl's classpath (requires cemerick.pomegranate)?"))))
   (message "Searching...")
   (let ((spec (lcl/get-latest-clojure-library package)))
@@ -52,7 +57,7 @@
         (progn (message (concat "Found " spec))
                (insert spec)
                (when inject (lcl/add-clojure-dependency spec)))
-      (error "Can't find %s, are you sure you have the correct spelling?" package))))
+      (error "Can't find %s, are you sure you have the correct spelling? Do you have leiningen and the lein-ancient plugin set up?" package))))
 
 (provide 'latest-clojure-libraries)
 ;;; latest-clojure-libraries.el ends here
